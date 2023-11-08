6b6debbe4e084deabf627f40b924490c70714c33
jpmmatias <jpmmatias2018@outlook.com>
2023-11-07 22:51:55 -0300

    feat: populating categories with localstorage

--- codigo-fonte/js/controllers/uiController.js
+++ codigo-fonte/js/controllers/uiController.js
@@ -1,3 +1,6 @@
+import { retryQuerySelector } from '../helpers';
+import { categoryRepository } from '../repositories/categoriesRepository';
+
 class UIController {
 	constructor() {
 		this.drawer = null;
@@ -22,6 +25,40 @@ class UIController {
 	toggleDrawer() {
 		this.drawer.classList.toggle('active');
 	}
+
+	populateCategoriesNav() {
+		const categories = categoryRepository.getAll();
+		retryQuerySelector('#categoriesNav', (element) => {
+			const navItems = categories
+				.map(
+					(category) => `
+			<li>
+				<a href="/eventos?categoria=${category.name}">
+				 <img src="${category.icon}" alt="${category.name}" />
+				</a>
+			</li>
+		`
+				)
+				.join('');
+			element.innerHTML += navItems;
+		});
+	}
+
+	populateFilterCategoriesSelect() {
+		const categories = categoryRepository.getAll();
+		const categoriesOptions = categories.map(
+			(category) => `<option value="${category.name}">${category.name}</option>`
+		);
+		const options = [
+			...categoriesOptions,
+			'<option disabled selected>Categoria</option>',
+		].join('');
+
+		retryQuerySelector('#filterCategory', (element) => {
+			element.innerHTML = options;
+		});
+	}
 }
 
-export { UIController };
+const uiController = new UIController();
+export { UIController, uiController };
